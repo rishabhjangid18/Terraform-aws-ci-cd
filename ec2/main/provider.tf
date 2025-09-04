251936e7dfc9aa83b2c3018c8f014a1a339c2837
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-04 17:59:44 +0530

    Create provider.tf

---
+++ ec2/main/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1" # must match where you want S3 + DynamoDB
+}
